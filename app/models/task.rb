4f90c91eeeecdcd860eaac23113ee75a01139615
Will Piers <wpiers0405@gmail.com>
2013-03-12 15:19:44 -0500

    scaffolded the Tasks resource. added some AJAX for the removal of a task.

---
+++ app/models/task.rb
@@ -0,0 +1,5 @@
+class Task < ActiveRecord::Base
+  attr_accessible :description
+
+  validates_presence_of :description
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Todo::Application.routes.draw do
+
+  root to: "tasks#index"
+
+  resources :tasks
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
